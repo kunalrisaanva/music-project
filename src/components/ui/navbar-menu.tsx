188bb4ec1a49edd2a4863001681bb6ae358dbabb
kunalrisaanva <kunalrisaanva12@gmail.com>
2025-03-28 13:58:54 +0530

    Enhance type safety in HoveredLink and StickyScroll components by specifying prop types

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -109,7 +109,9 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
+import { LinkProps } from "next/link";
+
+export const HoveredLink = ({ children, ...rest }: LinkProps & { children: React.ReactNode }) => {
   return (
     <Link
       {...rest}
